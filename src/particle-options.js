bbd9f19190622efca8878bc232afb631042162da
jameswhitney <33575806+jameswhitney@users.noreply.github.com>
2020-10-14 16:52:16 -0400

    Adds changes to particles-option.js

--- src/particle-options.js
+++ src/particle-options.js
@@ -1,10 +1,17 @@
 export const particleOptions = {
   particles: {
     number: {
-      value: 80,
+      value: 60,
     },
     size: {
       value: 4,
+    },
+  },
+  line_linked: {
+    shadow: {
+      enable: true,
+      color: "#0000",
+      blur: 5,
     },
   },
 };
